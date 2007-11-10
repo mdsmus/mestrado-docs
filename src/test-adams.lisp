92ebf2b474afdbef3c22d6187c67e50f2b1f8f57
Marcos di Silva <contato@marcosdisilva.net>
2007-11-10 11:14:38 -0300

    novos testes para inclinacao

--- src/test-adams.lisp
+++ src/test-adams.lisp
@@ -20,12 +20,14 @@
   )
 
 (define-test inclinacoes-contorno
+  (assert-equal '(0) (inclinacoes-contorno '((0 0) (1 0))))
   (assert-equal '(1) (inclinacoes-contorno '((0 0) (1 1))))
   (assert-equal '(1 -2) (inclinacoes-contorno '((0 4) (1 5) (2 3))))
   (assert-equal '(1 -2 -2 3 5 -6 -1) (inclinacoes-contorno '((0 4) (1 5) (2 3) (3 1) (4 4) (5 9) (6 3) (7 2))))
   )
 
 (define-test inclinacoes-contorno-positivo-negativo
+  (assert-equal '(0) (inclinacoes-contorno-positivo-negativo '((0 0) (1 0))))
   (assert-equal '(1) (inclinacoes-contorno-positivo-negativo '((0 0) (1 1))))
   (assert-equal '(1 -1) (inclinacoes-contorno-positivo-negativo '((0 4) (1 5) (2 3))))
   (assert-equal '(1 -1 -1 1 1 -1 -1) (inclinacoes-contorno-positivo-negativo '((0 4) (1 5) (2 3) (3 1) (4 4) (5 9) (6 3) (7 2))))
