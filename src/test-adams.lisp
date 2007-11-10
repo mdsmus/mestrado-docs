c7f3998145d3d3e1f3396b0a8dae4ada529366b2
Marcos di Silva <contato@marcosdisilva.net>
2007-11-10 11:06:50 -0300

    novos testes para reducao de contorno

--- src/test-adams.lisp
+++ src/test-adams.lisp
@@ -2,7 +2,21 @@
 (use-package :lisp-unit)
 
 (define-test reduzir-contorno
-  (assert-equal '((0 4) (3 0) (5 9) (7 2)) (reduzir-contorno '((0 4) (1 5) (2 3) (3 0) (4 6) (5 9) (6 1) (7 2))))
+  (assert-equal '((0 1) (1 0)) (reduzir-contorno '((0 1) (1 0))))
+  (assert-equal '((0 0) (1 1) (2 0)) (reduzir-contorno '((0 0) (1 1) (2 0))))
+  (assert-equal '((0 0) (2 2)) (reduzir-contorno '((0 0) (1 1) (2 2))))
+  (assert-equal '((0 1) (1 0) (2 1)) (reduzir-contorno '((0 1) (1 0) (2 1))))
+  (assert-equal '((0 0) (1 0)) (reduzir-contorno '((0 0) (1 0))))
+  ;; escolher resposta do teste abaixo
+  (assert-equal '((0 0) (1 2) (4 0)) (reduzir-contorno '((0 0) (1 2) (2 1) (3 2) (4 0))))
+  (assert-equal '((0 0) (3 2) (4 0)) (reduzir-contorno '((0 0) (1 2) (2 1) (3 2) (4 0))))
+  (assert-equal '((0 0) (1 2) (3 2) (4 0)) (reduzir-contorno '((0 0) (1 2) (2 1) (3 2) (4 0))))
+  ;;
+  ;; escolher resposta do teste abaixo
+  (assert-equal '((0 2) (1 4) (2 0) (5 2)) (reduzir-contorno '((0 2) (1 4) (2 0) (3 4) (4 0) (5 2))))
+  (assert-equal '((0 2) (2 0) (3 4) (5 2)) (reduzir-contorno '((0 2) (1 4) (2 0) (3 4) (4 0) (5 2))))
+  (assert-equal '((0 2) (3 4) (4 0) (5 2)) (reduzir-contorno '((0 2) (1 4) (2 0) (3 4) (4 0) (5 2))))
+  ;;
   )
 
 (define-test inclinacoes-contorno
