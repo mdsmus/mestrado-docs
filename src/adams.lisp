c0c33550837797504fbfc7dd41e6b73bc4131172
Marcos di Silva <contato@marcosdisilva.net>
2007-11-09 21:45:51 -0300

    funcao para reduzir contornos
    * falta remover repeticoes

--- src/adams.lisp
+++ src/adams.lisp
@@ -1,10 +1,13 @@
 (in-package #:contornos)
 
-;; FIXME: inserir pontos mais alto e mais baixo
+;; FIXME: remover repeticoes
 (defun reduzir-contorno (pares)
   "reduz pontos de um contorno a ponto inicial, final, ponto mais
 alto e mais baixo"
-  (list (first pares) (last pares)))
+  (let* ((primeiro (first pares))
+         (ultimo (first (reverse pares)))
+         (mais-agudo (first (sort pares #'> :key #'second)))
+         (mais-grave (first (sort pares #'< :key #'second))))
+    (sort (list primeiro mais-agudo mais-grave ultimo) #'< :key #'first)))
 
-;; teste
 ;; (reduzir-contorno '((0 4) (1 5) (2 3) (3 1) (4 4) (5 9) (6 3) (7 2)))
