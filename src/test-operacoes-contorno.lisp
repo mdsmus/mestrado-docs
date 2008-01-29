62df119bdc55f4eae1aa284a9e6880e852bd2a16
Marcos di Silva <contato@marcosdisilva.net>
2008-01-29 13:53:07 -0300

    testes de unidade com funcoes novas

--- src/test-operacoes-contorno.lisp
+++ src/test-operacoes-contorno.lisp
@@ -89,4 +89,14 @@
   (assert-equal '((0 1) (1 3) (2 4) (3 5) (4 0)) (remover-duplicatas '((0 1) (1 3) (0 1) (2 4) (3 5) (4 0) (4 0))))
   )
 
-;; inserir remover-alturas-repetidas
+(define-test remover-alturas-repetidas
+  (assert-equal '((0 0)) (remover-alturas-repetidas '((0 0) (1 0) (2 0) (3 0))))
+  (assert-equal '((0 0) (1 2) (2 1) (5 4)) (remover-alturas-repetidas '((0 0) (1 2) (2 1) (3 0) (4 2) (5 4)))))
+
+(define-test contorno-com-duracao->contorno-simples
+  (assert-equal '(1 0) (contorno-com-duracao->contorno-simples '((0 1) (1 0))))
+  (assert-equal '(1 0 2) (contorno-com-duracao->contorno-simples '((0 1) (1 0) (2 2)))))
+
+(define-test contorno-simples->contorno-com-duracao
+  (assert-equal '((0 1) (1 0)) (contorno-simples->contorno-com-duracao '(1 0)))
+  (assert-equal '((0 1) (1 3) (2 2) (3 0)) (contorno-simples->contorno-com-duracao '(1 3 2 0))))
