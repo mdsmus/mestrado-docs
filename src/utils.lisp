f372c1feb1fa339027755421a4a21a02c2f8a396
Marcos di Silva <contato@marcosdisilva.net>
2008-02-01 15:34:30 -0300

    orientacao a objetos: criacao de classes, parametros e instancias

--- src/utils.lisp
+++ src/utils.lisp
@@ -14,3 +14,38 @@
 (defun concat (&rest string)
   "Concatena strings."
   (apply #'concatenate 'string string))
+
+;;;; orientação a objetos
+
+;; definição de classes
+
+(defclass ponto ()
+    ((args :accessor args :initarg :args)))
+
+(defclass contorno-com-duracao ()
+  ((args :accessor args :initarg :args)))
+
+(defclass contorno-simples ()
+  ((args :accessor args :initarg :args)))
+
+;; definição de parâmetros
+
+(defparameter ponto
+  (make-instance 'ponto :args '(1 0 2)))
+
+(defparameter contorno-com-duracao
+  (make-instance 'contorno-com-duracao :args '((0 1) (0 0))))
+
+(defparameter contorno-simples
+  (make-instance 'contorno-simples :args '(1 0 2)))
+
+;; make instance
+
+(defun make-ponto (args)
+  (make-instance 'ponto :args args))
+
+(defun make-contorno-com-duracao (args)
+  (make-instance 'contorno-com-duracao :args args))
+
+(defun make-contorno-simples (args)
+  (make-instance 'contorno-simples :args args))
