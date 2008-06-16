b246dd7ee08de6201d6d3b74bb23a65ee36780a9
Marcos di Silva <mdsmus@gmail.com>
2008-06-16 14:29:51 -0300

    Função para retornar contornos em um arquivo /tmp/contornos.txt
    * incompleta

--- opus-6-composicao-mestrado/planos-e-materiais/operacoes-contornos.lisp
+++ opus-6-composicao-mestrado/planos-e-materiais/operacoes-contornos.lisp
@@ -1,5 +1,6 @@
 (in-package #:goiaba)
 
+;; imprime gráficos
 (let* ((original (make-contorno-duracao-lista '((0 5) (1 3) (2 4) (3 1) (4 2) (5 0))))
        (inversao (inverter original))
        (retrogrado (retrogradar original))
@@ -31,3 +32,23 @@
       (plot-contorno 220 550 "irotação4" irotacao4 :width 100 :height 100 :font-size 5.0)
       (plot-contorno 400 550 "irotação5" irotacao5 :width 100 :height 100 :font-size 5.0))
     (write-document "/tmp/foo.pdf")))
+
+;; retorna operações simples de contorno original
+(let* ((original (make-contorno-duracao-lista '((0 5) (1 3) (2 4) (3 1) (4 2) (5 0))))
+       (inversao (inverter original))
+       (retrogrado (retrogradar original))
+       (rotacao1 (rotacionar original 1))
+       (rotacao2 (rotacionar original 2))
+       (rotacao3 (rotacionar original 3))
+       (rotacao4 (rotacionar original 4))
+       (rotacao5 (rotacionar original 5)))
+  (format nil "~a" (list
+                    original
+                    inversao
+                    retrogrado
+                    rotacao1
+                    rotacao2
+                    rotacao3
+                    rotacao4
+                    rotacao5)))
+
