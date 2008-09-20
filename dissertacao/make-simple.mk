f0a088b4714d885039568ad56e2f09ccaf346c0c
Marcos di Silva <mdsmus@gmail.com>
2008-09-20 16:40:22 -0300

    Insere dependência de arquivos

--- dissertacao/make-simple.mk
+++ dissertacao/make-simple.mk
@@ -1,7 +1,20 @@
 NAME = dissertacao-simples
-TEXSRCS = body-pt.tex abstract.tex
+TEXSRCS = body-pt.tex abstract.tex resumo.tex newcommand.tex lista-bibliografia.tex
+
+SCOREDIR = ../opus-6-composicao-mestrado
 
 OTHER += $(LILY_EPS)
 
 -include ~/lib/make/latex.mk
 -include ~/repositorios/genos-repos/lib/make/latex.mk
+
+light-clean:
+	rm -f dissertacao-default.{aux,lo*,ps,pdf,bl*,dvi,to*,bb*}
+	rm -f dissertacao-simples.{aux,lo*,ps,pdf,bl*,dvi,to*,bb*}
+
+cria-partitura:
+	rm -f figs/score-dissertacao*.eps
+	lilypond -b eps -dno-gs-load-fonts -dinclude-eps-fonts -I $(SCOREDIR)/partitura/notas/ -I $(SCOREDIR)/script/temp -I $(SCOREDIR)/script/systemfiles $(SCOREDIR)/partitura/score-dissertacao.ly
+	mv score-dissertacao*.eps figs
+	rm score-dissertacao*
+	sh gera-paginas-composicao.sh
