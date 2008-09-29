f97b19fa62bab2ebbe49946af1fc2a84d0cd9ddc
Marcos di Silva <mdsmus@gmail.com>
2008-09-29 15:46:41 -0300

    Organiza lilypond-book e resolve problema com sistemas

--- dissertacao/arruma-lytex.sh
+++ dissertacao/arruma-lytex.sh
@@ -2,6 +2,6 @@
 
 f=lily*-systems.tex
 g=paginas-composicao.tex
-sed 's/includegraphics{/includegraphics{figs\//g' $f > $f-new ; mv $f-new $f
+cat $f | sed 's/includegraphics{/includegraphics{figs\//g' | sed 's/linebreak/linebreak\ \\vspace{36pt}/g' > $f-new ; mv $f-new $f
 sed '1,3 d' $g | sed '$ d' > $g-new ; mv $g-new $g
 mv lily-*.eps figs
