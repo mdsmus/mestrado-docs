59767195a8d12c55851b3458354c670d6ad651db
Marcos di Silva <mdsmus@gmail.com>
2008-09-29 05:29:46 -0300

    Revert "Opção (feia) para usar lytex"
    This reverts commit f8d86ad310ef4d4fc7db07e27af59ed21ae547a3.

--- dissertacao/arruma-lytex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-f=lily*-systems.tex
-g=paginas-composicao.tex
-sed 's/includegraphics{/includegraphics{figs\//g' $f > $f-new ; mv $f-new $f
-sed '1,3 d' $g | sed '$ d' > $g-new ; mv $g-new $g
-mv lily-*.eps figs
