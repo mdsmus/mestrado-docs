55ee1734a5e09ed5adc1249e7e8d271ee5681715
Marcos di Silva <mdsmus@gmail.com>
2008-10-14 06:44:00 -0300

    Gambiarra melhora espaçamento entre sistemas
    * remove espaçamento entre título e primeiro sistema
    * diminui espaçamento entre 1. e 2. sistemas
    * diminui espaçamento dos outros sistemas

--- dissertacao/arruma-lytex.sh
+++ dissertacao/arruma-lytex.sh
@@ -2,6 +2,15 @@
 
 f=lily*-systems.tex
 g=paginas-composicao.tex
-cat $f | sed 's/includegraphics{/includegraphics{figs\//g' | sed 's/linebreak/linebreak\ \\vspace{36pt}/g' > $f-new ; mv $f-new $f
+l="  \linebreak"
+
+sed '1,2 !d' $f > $f-new
+echo $l >> $f-new
+sed '4,8 !d' $f >> $f-new
+echo $l \\vspace{20pt} >> $f-new
+sed '10,$!d' $f | sed 's/includegraphics{/includegraphics{figs\//g' | sed 's/linebreak/linebreak\ \\vspace{22pt}/g' >> $f-new ; mv $f-new $f
+
 sed '1,3 d' $g | sed '$ d' > $g-new ; mv $g-new $g
-mv lily-*.eps figs
+
+mv lily-*.eps figs 2> /dev/null
+
